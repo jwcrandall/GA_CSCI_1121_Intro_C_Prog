dd463ec33b42e4e9b44324f231cdaa13153af7e4
Joseph Crandall <josephcrandall@me.com>
2018-01-31 15:42:21 -0500

    printing ints and characters

---
+++ w3_01-29/printVarAsCharAndInt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//Print two values as characters and integers
+int main(){
+  //Delcare and initialize variables
+  char ch = 'a';
+  int i = 56;
+
+  //Print both variables as characters
+  printf("Value of ch is %c; Value of i is %c \n", ch, i);
+  //Print both variables as integers
+  printf("Value of ch is %i; Value of i is %i \n", ch, i);
+  //Exit program
+  return 0;
+}
+
