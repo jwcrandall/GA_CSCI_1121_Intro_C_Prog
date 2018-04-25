08249c967bd13c0c95f741051669f48e5efd210a
Joseph Crandall <josephcrandall@me.com>
2018-04-25 14:43:26 -0400

    Working Problem 2

--- w15_04-23/LPS/problem2.c
+++ w15_04-23/LPS/problem2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-//Create a function to find out the class
+//Create a function to find the class
 char findClass(char str[]){
   //Store the first octet in an arr[]
   char arr[4];
@@ -19,49 +19,29 @@ char findClass(char str[]){
     i--;
   }
   //Write for Class A
-  if(ip >= 1 && ip <= 126){
-    return 'A';
-  }
+  if(ip >= 1 && ip <= 126) return 'A';
   //Write for Class B
-  else if(ip >= 128 && ip <= 191){
-    return 'B';
-  }
+  else if(ip >= 128 && ip <= 191) return 'B';
   //Write for Class C
-  else if (ip >= 192 && ip <= 223){
-    return 'C';
-  }
+  else if (ip >= 192 && ip <= 223) return 'C';
   //Write for Class D
-  else if (ip >= 224 && ip <= 239){
-    return 'D';
-  }
+  else if (ip >= 224 && ip <= 239) return 'D';
   //Write for Class E
-  else {
-    return 'E';
-  }
+  else return 'E';
 }
 //Create a function to separate network ID and host ID and to print them
 void separate(char str[], char ipClass){
   //Initialize network and host array to NULL
   char network[12], host[12];
   int k;
-  for(k = 0; k<12; k++){
-    network[k] = host[k] = '\0';
-  }
+  for(k = 0; k<12; k++) network[k] = host[k] = '\0';
   //For class A, only first octet is Network ID and rest are host ID
   if(ipClass == 'A'){
     int i = 0, j = 0;
-    while (str[j] != '.')
-    network[i++] = str[j++];
+    while (str[j] != '.') network[i++] = str[j++];
     i = 0;
     j++;
-    while (str[j] != '\0'){
-      network[i++] = str[j++];
-    }
-    i = 0;
-    j++;
-    while(str[j] != '\0'){
-      host[i++] = str[j++];
-    }
+    while (str[j] != '\0') network[i++] = str[j++];
     printf("Network ID is %s\n", network);
     printf("Host ID is %s\n", host);
   }
@@ -69,37 +49,29 @@ void separate(char str[], char ipClass){
   else if(ipClass == 'B'){
     int i = 0, j = 0, dotCount = 0;
     //Storing in network[] up to 2nd dot, dotCount keeps track of number of dots or octets that are passed
-      while(dotCount < 2){
-        network[i++] = str[j++];
-      }
-      if(str[j] == '.'){
-        dotCount++;
-      }
-      i = 0;
-      j ++;
-      while(str[j] != '\0'){
-        host[i++] = str[j++];
-      }
-      printf("Network ID is %s\n", network);
-      printf("Host ID is %s\n", host);
+    while(dotCount < 2){
+      network[i++] = str[j++];
+      if(str[j] == '.') dotCount++;
     }
+    i = 0;
+    j ++;
+    while(str[j] != '\0') host[i++] = str[j++];
+    printf("Network ID is %s\n", network);
+    printf("Host ID is %s\n", host);
+  }
   //for class C, first three octet are Network ID and rest are HOST ID
   else if(ipClass == 'C'){
     int i = 0, j = 0, dotCount = 0;
-    //storing in network[] up to  3rd dor, dotCount keeps track of number of dots or octets passed.
+    //storing in network[] up to  3rd dot, dotCount keeps track of number of dots or octets passed.
     while(dotCount < 3){
       network[i++] = str[j++];
-      if(str[j] == '.'){
-        dotCount++;
-      }
-      i = 0;
-      j++;
-      while (str[j] != '\0'){
-        host[i++] = str[j++];
-      }
-      printf("Network ID is %s\n", network);
-      printf("Host ID is %s\n", host);
+      if(str[j] == '.') dotCount++;
     }
+    i = 0;
+    j++;
+    while (str[j] != '\0') host[i++] = str[j++];
+    printf("Network ID is %s\n", network);
+    printf("Host ID is %s\n", host);
   }
   //Class D and E are not divided in network and host ID
   else{
