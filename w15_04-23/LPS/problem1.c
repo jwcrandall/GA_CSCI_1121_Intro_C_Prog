3c517fed0602d224b5c234ef5731d9b11149d722
Joseph Crandall <josephcrandall@me.com>
2018-04-25 16:04:04 -0400

    final project1

--- w15_04-23/LPS/problem1.c
+++ w15_04-23/LPS/problem1.c
@@ -5,35 +5,40 @@ int main(void){
   printf("Enter IP address (x.x.x.x): ");
   while((ch = getchar()) != '\n' && ch != EOF){
     if(ch < '0' || ch > '9'){
-    if(ch == '.'){
-    dots++;
-    if(temp != -1){
-    if(temp > 255){
-    printf("Error: The value of each byte should be in [0,255]\n");
-    return 0;
-    }
-    bytes ++;
-    temp = -1;
-    } //The value -1 mean scurre nt address byte is checked
-    }
+      if(ch == '.'){
+        dots++;
+        if(temp != -1){
+          if(temp > 255){
+            printf("Error: The value of each byte should be in [0,255]\n");
+            return 0;
+          }
+          bytes ++;
+          temp = -1;
+        } //The value -1 mean scurre nt address byte is checked
+      }
     else{
-    printf("Error: Acceptable chars are only digits and dots\n");
-    return 0;
+      printf("Error: Acceptable chars are only digits and dots\n");
+      return 0;
     }
+  }
+  else{
+    if(temp == -1) temp = 0; //Make it 0, to start checking the next adddress byte
+      temp = 10*temp + (ch - '0');
     }
-    else{
-      if(temp == -1) temp = 0; //Make it 0, to start checking the next adddress byte
-        temp = 10*temp + (ch - '0');
-    }}
-    if(temp != -1)
-      { //Check the value of the last address byte
+  }
+  if(temp != -1)
+    { //Check the value of the last address byte
       if(temp > 255){
         printf("Error: The value of each byte should be in [0, 255]\n");
         return 0;
       }
       bytes++;
     }
-  if(dots != 3 || bytes != 4) printf("Error: The IP format shoudl be x.x.x.x\n");
-  else printf("The input address is a valid IPv4 address\n");
+  if(dots != 3 || bytes != 4){
+    printf("Error: The IP format shoudl be x.x.x.x\n");
+  }
+  else{
+    printf("The input address is a valid IPv4 address\n");
+  }
   return 0;
 }
